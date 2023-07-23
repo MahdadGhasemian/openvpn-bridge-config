e10091f1a1c40e7807ea700d29ee75c883851bf3
Mahdad Ghasemian <mahdad.ghasemian@gmail.com>
2023-07-23 05:42:20 +0330

    install-bridge.sh

--- install-bridge.sh
+++ install-bridge.sh
@@ -3,16 +3,17 @@
 ## Write service file
 cat <<EOF > /etc/systemd/system/tunnel-to-upstream.service
 [Unit]
-Description=tunneling to upstream server
+Description=Setup a secure tunnel to $4
 Documentation=https://mahdad.me
-After=network-online.target
-Wants=network-online.target
+After=network.target
 
 [Service]
 Type=simple
 User=root
 Group=root
-ExecStart=/usr/bin/bash ssh -p$1 -f -N -L *:$2:localhost:$3 root@$4
+ExecStart=/usr/bin/ssh -p$1 -f -N -L *:$2:localhost:$3 root@$4
+RestartSec=5
+Restart=always
 
 [Install]
 WantedBy=multi-user.target
